1da759993848e92c7e4bf657083c91945ca3d24b
Kenway23 <rifkimuhamad6635@gmail.com>
2025-12-13 22:31:00 +0700

    update connection

--- config/database.php
+++ config/database.php
@@ -7,14 +7,13 @@ $username = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 
 try {
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 5,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ];
-
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
-    $pdo = new PDO($dsn, $username, $password, $options);
+
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 5
+    ]);
+
 } catch (PDOException $e) {
-    die("Koneksi DB gagal");
+    die("Database error");
 }
