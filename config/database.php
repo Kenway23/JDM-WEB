4fc5c37d5e91806c279e8f4be3a22e0cc053f11b
Kenway23 <rifkimuhamad6635@gmail.com>
2025-12-16 15:21:03 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,12 +1,21 @@
 <?php
-$host = "localhost";
-$dbname = "garasi_muda";
-$username = "root";
-$password = "";
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
+$dbname = getenv("MYSQLDATABASE");
+$username = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+    // echo "Koneksi berhasil";
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
 }
