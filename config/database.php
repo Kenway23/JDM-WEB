78e0004a90654ca56258fee65998ef1541f15220
Kenway23 <rifkimuhamad6635@gmail.com>
2025-12-16 14:56:22 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,20 +1,13 @@
 <?php
-
-$host = getenv("MYSQLHOST");
-$port = getenv("MYSQLPORT");
-$dbname = getenv("MYSQLDATABASE");
-$username = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
+$host = "localhost";
+$dbname = "garasi_muda";
+$username = "root";
+$password = "";
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
-
-    $pdo = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 5
-    ]);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // JANGAN echo di production
-    error_log($e->getMessage());
-    exit;
+    die("Koneksi gagal: " . $e->getMessage());
 }
+?>
